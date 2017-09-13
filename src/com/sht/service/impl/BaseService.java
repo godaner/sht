ece1b2dd2ebd0d78c7d0bb50cf50478ebcafacfa
Kor Zhang <1138829222@qq.com>
2017-09-13 09:25:28 +0800

    更新注释
    更新注释

--- src/com/sht/service/impl/BaseService.java
+++ src/com/sht/service/impl/BaseService.java
@@ -1,5 +1,6 @@
 package com.sht.service.impl;
 
+import com.sht.util.GStatic;
 import com.sht.util.GUtil;
 
 /**
@@ -11,6 +12,17 @@ import com.sht.util.GUtil;
  * @date 2017年9月12日 上午11:15:46
  * @version 1.0
  */
-public class BaseService extends GUtil {
-
+public class BaseService implements GStatic {
+	/**
+	 * Title:Util
+	 * <p>
+	 * Description:工具类,使用内部类继承利于拓展;
+	 * <p>
+	 * @author Kor_Zhang
+	 * @date 2017年9月13日 上午8:32:45
+	 * @version 1.0
+	 */
+	protected class Util extends GUtil {
+		
+	} 
 }
