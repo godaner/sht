5522b135851f32d002b15d7abd465f83077f2a47
lemonApple <1305299813@qq.com>
2017-09-21 20:05:30 +0800

    更新GoodsAction
    更新GoodsAction

--- src/com/sht/goods/action/GoodsAction.java
+++ src/com/sht/goods/action/GoodsAction.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.sht.goods.po.CustomGoods;
-import com.sht.goods.service.impl.GoodsService;
+import com.sht.goods.service.GoodsServiceI;
+
 
 
 /**
@@ -20,7 +21,7 @@ import com.sht.goods.service.impl.GoodsService;
  */
 @Controller
 @Scope("prototype")
-public class GoodsAction extends GBaseAction<CustomGoods,GoodsService> {
+public class GoodsAction extends GBaseAction<CustomGoods,GoodsServiceI> {
 	
 	/**
 	 * Title:login
