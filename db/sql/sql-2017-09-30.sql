8898d5ce347f00eb110338b596a830611ca39cc8
godaner <1138829222@qq.com>
2017-09-30 15:57:07 +0800

    更新数据库,添加goods的-55状态
    更新数据库,添加goods的-55状态

--- db/sql/sql-2017-09-30.sql
+++ db/sql/sql-2017-09-30.sql
@@ -28,3 +28,22 @@ ALTER TABLE goods  RENAME COLUMN RETURNMONEYBILL TO refuseReturnMoneyBill;
 COMMENT ON COLUMN goods. refuseReturnMoneyBill
 IS '商家拒绝退款的凭证'
 
+
+
+/**
+ * 15:55	ignoreable
+ */
+/*添加-4状态*/
+COMMENT ON COLUMN goods.status
+IS '-6:待审核状态,(不可以被显示,不可以购买)
+          -7:审核未通过,(不可以被显示,不可以购买)
+          0:审核通过,(可以被显示,可以购买)
+          1:购买了且待发货,
+          2:已发货,
+          -1:买家收货后交易正常结束,
+          -2:卖家取消了出售本商品,
+        -3:买家取消购买本商品,
+        -4:管理员取消购买本商品,
+        -5:管理员删除本商品,
+          -8:买家申请退款,
+		      -9:退款成功（失败则保持-1状态）'
