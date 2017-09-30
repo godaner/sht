0d1c24614b97910d6e8a58558126e7c2e91eec3b
godaner <1138829222@qq.com>
2017-09-30 11:20:52 +0800

    添加商品字段；添加商品状态：-8:买家申请退款,  -9:退款成功（失败则保持-1状态）
    添加商品字段；添加商品状态：-8:买家申请退款,  -9:退款成功（失败则保持-1状态）

--- src/com/sht/util/Static.java
+++ src/com/sht/util/Static.java
@@ -181,12 +181,14 @@ public interface Static {
 	 */
 	public interface GOODS_STAUS{
 		/*状态:,
-		-6:待审核状态,(不可以被显示,不可以购买)
-		0:审核通过,(可以被显示,可以购买)
-		1:购买了且待发货,
-		2:已发货,
-		-1:买家收货后交易正常结束,
-		-5:管理员删除本商品*/
+			-6:待审核状态,(不可以被显示,不可以购买)
+			0:审核通过,(可以被显示,可以购买)
+			1:购买了且待发货,
+			2:已发货,
+			-1:买家收货后交易正常结束,
+			-5:管理员删除本商品,
+			-8:买家申请退款,
+      		-9:退款成功（失败则保持-1状态）*/
 		/**
 		 * 待审核
 		 */
@@ -208,12 +210,20 @@ public interface Static {
 		 * 买家收货后正常结束
 		 */
 		static final Short BUYER_RECEIVED_AND_FINISHED = -1;
+
 		/**
 		 * 管理员删除本的商品
 		 */
 		static final Short ADMIN_DELETE = -5;
-	
-		
+		/**
+		 * 买家申请退款
+		 */
+		static final Short WAIT_RETURN_MONEY = -8;
+
+		/**
+		 * 退款成功（失败则保持-1状态）
+		 */
+		static final Short RETURN_MONEY_SUCCESS = -9;
 		
 	}
 	
