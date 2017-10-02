0964ac2104a23f6411181b0d5d57b775cd16e5d4
godaner <1138829222@qq.com>
2017-10-02 15:18:09 +0800

    更新数据库
    更新数据库

--- db/sql/sql-2017-10-02.sql
+++ db/sql/sql-2017-10-02.sql
@@ -5,7 +5,14 @@
 	全部模块都需立即更新:alls
 	可忽略的,暂时可不更新的:ignoreable
 /**
- * 21:33	alls
+ * 未知	ignoreable
  */
 /*增加roles字段DESCRIPTION*/
 ALTER TABLE ROLES ADD (DESCRIPTION NVARCHAR2(255))
+
+/**
+ * 15:17	ignoreable
+ */
+/*增加roles字段DESCRIPTION*/
+  COMMENT ON COLUMN roles.STATUS
+  IS'是否可用,1:可用,0:删除,-1:冻结'
